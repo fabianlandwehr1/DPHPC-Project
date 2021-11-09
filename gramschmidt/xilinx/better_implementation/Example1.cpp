17cebb444a034273e230748a90158f2e18b5652a
mezhang111 <91820741+mezhang111@users.noreply.github.com>
2021-11-09 21:32:49 +0100

    Add files via upload
    small changes

--- gramschmidt/xilinx/better_implementation/Example1.cpp
+++ gramschmidt/xilinx/better_implementation/Example1.cpp
@@ -4,9 +4,8 @@
 constexpr int N = 64;
 
 void gramschmidt_naive(float *A, float *Q, float *R) {
-
   for(int i=0; i<N; i++){
-    float squared_norms[N];
+    float squared_norms[N]; 
     for(int j=0; j<N; j++){
       float val = A[j*N+i];
       for(int k=i; k<N; k++){
@@ -16,10 +15,10 @@ void gramschmidt_naive(float *A, float *Q, float *R) {
         #pragma HLS DEPENDENCE variable=squared_norms false
       }// = <a_i, a_j>
     }
-    float norm = hls::sqrt(squared_norms[i]); //|a_i|
+    float norm = hls::rsqrt(squared_norms[i]); //1/|a_i|
     for(int k=0; k<N; k++){
       #pragma HLS PIPELINE II = 1
-      Q[k*N+i] = A[k*N+i]/norm;
+      Q[k*N+i] = A[k*N+i]*norm;
     } 
     float squared_norm = squared_norms[i];
 
@@ -34,22 +33,9 @@ void gramschmidt_naive(float *A, float *Q, float *R) {
 
     for(int j=i+1; j<N; j++){
       #pragma HLS PIPELINE II = 1
-      R[i*N+j] = squared_norms[j]/norm;
-      #pragma HLS DEPENDENCE variable=R false
+      R[i*N+j] = squared_norms[j]*norm;
     }
-    /*
-    for(int j=i+1; j<N; j++){
-      float factor = squared_norms[j]/squared_norm;
-      for(int k=0; k<N; k++){
-        #pragma HLS PIPELINE II = 1
-        A[k*N+j] -= factor*A[k*N+i];
-        #pragma HLS DEPENDENCE variable=A false
-      }
-      R[i*N+j] = factor*norm;
-      #pragma HLS DEPENDENCE variable=R false
-    }
-    */
-    
+
   }
 }
 
