44eb4a1ed85949cc0b16eef4d777b16f31b925ec
G Rahul Kranti Kiran <krantikiran.68@gmail.com>
2021-11-10 00:25:28 +0100

    Added trivial kernel code to Durbin.cpp. Need to do: 1) Reordering if possible 2) Vectorization (Use example 6 from the tutorial)

--- durbin/xilinx/Durbin.cpp
+++ durbin/xilinx/Durbin.cpp
@@ -4,13 +4,41 @@
 
 using hlslib::Stream;
 
-void ProcessingElement(float const r[], float y[]) {
+void ProcessingElement(float const r[N], float y[N]) {
+  
+  float beta = 1.0;
+  float alpha = -r[0];
+  y[0] = -r[0];
 
-  y[0] = r[0]*r[0];
+  for (int i=1;i<N;i++)
+  {
+
+    #pragma HLS PIPELINE II=1
+    beta *= (1.0 - alpha * alpha);
+    float dotProdAccumulator = 0.0;
+
+    for(int j=0;j<i;j++)
+    {
+      #pragma HLS PIPELINE II=3
+      dotProdAccumulator += y[j] * r[i-1 - j];
+    } 
+    
+    #pragma HLS PIPELINE II=1
+    alpha = -1 * (r[i] + dotProdAccumulator) / beta;
+
+    for(int j=0;j<i;j++)
+    {
+      #pragma HLS PIPELINE II=19
+      y[j] += alpha * y[i-1 -j];
+    } 
+
+    #pragma HLS PIPELINE II=1
+    y[i] = alpha;
+  }
 
 }
 
-void Durbin(float const r[], float y[]) {
+void Durbin(float const *r, float *y) {
 
   #pragma HLS INTERFACE m_axi port=r offset=slave bundle=gmem0
   #pragma HLS INTERFACE m_axi port=y offset=slave bundle=gmem1
