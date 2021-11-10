ad44feee1aaec47492a38134bcb64fb681d4581e
Fabian Landwehr <fabianlandwehr@Fabians-Air.fritz.box>
2021-11-10 23:21:46 +0100

    Squashed commit of the following: Add multiple gramschmidt implementations
    commit 740a59277611af1d4a8aaeca067f4630b9fc8732
    Author: mezhang111 <91820741+mezhang111@users.noreply.github.com>
    Date:   Wed Nov 10 17:07:55 2021 +0100
    
        third one. 20% slower than the second one.
    
    commit 17cebb444a034273e230748a90158f2e18b5652a
    Author: mezhang111 <91820741+mezhang111@users.noreply.github.com>
    Date:   Tue Nov 9 21:32:49 2021 +0100
    
        Add files via upload
    
        small changes
    
    commit b5dda6766220efcf2e6c9577e122cb49ab8382c8
    Author: mezhang111 <91820741+mezhang111@users.noreply.github.com>
    Date:   Tue Nov 9 19:12:42 2021 +0100
    
        decreased depth of memory accesses. 7x faster
    
    commit 96dc1ccfa6baab96d7646ac22c5207ffbf908f72
    Author: mezhang111 <91820741+mezhang111@users.noreply.github.com>
    Date:   Tue Nov 9 18:44:16 2021 +0100
    
        6x faster. Less Data Dependencies.

--- gramschmidt/xilinx/better_implementation/Example1.cpp
+++ gramschmidt/xilinx/better_implementation/Example1.cpp
@@ -1,31 +1,41 @@
 #include <stdio.h>
 #include <math.h> 
+#include "hls_math.h"
 constexpr int N = 64;
 
 void gramschmidt_naive(float *A, float *Q, float *R) {
-
   for(int i=0; i<N; i++){
-    float squared_norms[N];
-    for(int j=i; j<N; j++){
-      float squared_norm = 0.0;
-      for(int k=0; k<N; k++){
-        float val1 = A[k*N+i];
-        float val2 = A[k*N+j];
-        squared_norm = squared_norm + val1*val2;
-      }
-      squared_norms[j] = squared_norm; //norm = <a_i, a_j>
+    float squared_norms[N]; 
+    for(int j=0; j<N; j++){
+      float val = A[j*N+i];
+      for(int k=i; k<N; k++){
+        #pragma HLS PIPELINE II = 1
+        float val2 = A[j*N+k];
+        squared_norms[k] += val*val2; //row-wise access instead column-wise, so we have no data dependencies in the inner loop
+        #pragma HLS DEPENDENCE variable=squared_norms false
+      }// = <a_i, a_j>
     }
-    float norm = squared_norms[i]; //|a_i|
+    float norm = hls::rsqrt(squared_norms[i]); //1/|a_i|
     for(int k=0; k<N; k++){
-      Q[k*N+i] = A[k*N+i]/norm;
+      #pragma HLS PIPELINE II = 1
+      Q[k*N+i] = A[k*N+i]*norm;
     } 
-    for(int j=i+1; j<N; j++){
-      float factor = squared_norms[j]/squared_norms[i];
-      for(int k=0; k<N; k++){
-        A[k*N+j] -= factor*A[k*N+i];
+    float squared_norm = squared_norms[i];
+
+    for(int k=0; k<N; k++){
+      float factor = A[k*N+i]/squared_norm;
+      for(int j=i+1; j<N; j++){
+        #pragma HLS PIPELINE II = 1
+        A[k*N+j] -= squared_norms[j]*factor;
+        #pragma HLS DEPENDENCE variable=A false
       }
-      R[i*N+j] = factor*norm;
     }
+
+    for(int j=i+1; j<N; j++){
+      #pragma HLS PIPELINE II = 1
+      R[i*N+j] = squared_norms[j]*norm;
+    }
+
   }
 }
 
