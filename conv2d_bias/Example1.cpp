ca4f68289830a1e1a4df6ab97cd22ff35376b39d
mezhang111 <91820741+mezhang111@users.noreply.github.com>
2021-11-22 20:01:35 +0100

    covid2d_naive

---
+++ conv2d_bias/Example1.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h> 
+#include "hls_math.h"
+constexpr int C_in = 3;
+constexpr int C_out = 16;
+constexpr int H = 32;
+constexpr int K = 2;
+constexpr int W = 32;
+constexpr int N = 8; 
+//input of size N*H*W*C_in, weights of size K*K*C_in*C_out, bias of size C_out, output of size N*H_out*W_out*C_out
+//output[i,:,:,j] = conv(input[i,:,:,:]*weights[:,:,:,j])+bias[j]
+//output[i,j,p,q] = sum(input[i,j:j+k,p:p+k,:]*weights[:,:,:,q])+bias[q]
+void conv2d(float const *input, float const *weights, float const *bias, float *output){
+  int H_out = N-K+1;
+  int W_out = N-K+1;
+  int i_o1 = H_out*W_out*C_out;  //offsets for indexing. i: input; w: weights; o: output 
+  int i_o2 = W_out*C_out;
+  int i_o3 = C_out;
+  int w_o1 = K*C_in*C_out;
+  int w_o2 = C_in*C_out;
+  int w_o3 = C_out;
+  int o_o1 = H_out*W_out*C_out;
+  int o_o2 = W_out*C_out;
+  int o_o3 = C_out;
+  for(int i=0; i<N; i++){
+    for(int j=0; j<H_out; j++){
+      for(int p=0; p<W_out; p++){
+        for(int q=0; q<C_out; q++){
+          float val = bias[q]
+          for(int k1=0; k1<K; k1++){
+            for(int k2=0; k2<K; k2++){
+              for(int c=0; c<C_in; c++){
+                val += input[i*i_o1+(j+k1)*i_o2+(p+k2)*i_o3+c]*weights[k1*w_o1+k2*w_o2+c*w_o3+q];
+              }
+            }
+          }
+          output[i*o_o1+j*o_o2+p*o_o3+q] = val;
+        }
+      }
+    }
+  }
+  
+}
+
