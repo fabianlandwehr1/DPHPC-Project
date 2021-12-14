ebb14f584a69b342a4717c1882f137a4c7acb7e6
mezhang111 <91820741+mezhang111@users.noreply.github.com>
2021-12-14 17:45:53 +0100

    more buffering

--- gramschmidt/xilinx/better_implementation/Entry.cpp
+++ gramschmidt/xilinx/better_implementation/Entry.cpp
@@ -1,7 +1,7 @@
 void gramschmidt_naive(float *A, float *Q, float *R);
 
 // Top-level entry function, not relevant for this example
-void Example1(float *A, float *Q, float *R) {
+void Example2(float *A, float *Q, float *R) {
   #pragma HLS INTERFACE m_axi port=A bundle=gmem0 offset=slave
   #pragma HLS INTERFACE m_axi port=Q bundle=gmem1 offset=slave
   #pragma HLS INTERFACE m_axi port=R bundle=gmem2 offset=slave
